463e54e3e082c3d295ee71d106e4e7710d464c61
Phoomparin Mano <phoomparin@gmail.com>
2025-08-22 16:07:49 +0800

    add param mode

--- ui/src/components/HeadlessController.tsx
+++ ui/src/components/HeadlessController.tsx
@@ -8,6 +8,15 @@ import {useSceneSwitcher} from '../hooks/useSceneSwitcher'
 import {useExhibitionScheduler} from '../hooks/useExhibitionScheduler'
 import {useExhibitionAutomator} from '../hooks/useExhibitionAutomator'
 
+import {
+  $offlineMode,
+  $exhibitionMode,
+  $videoMode,
+  $canPlay,
+} from '../store/exhibition'
+import {automator} from '../utils/exhibition/exhibition-automator'
+import {useEffect} from 'react'
+
 export const HeadlessController = () => {
   const cmd = useSceneSwitcher()
   const go = useNavigate()
@@ -24,6 +33,41 @@ export const HeadlessController = () => {
   // go to settings
   useHotkeys('CTRL + H', () => go({to: '/'}))
   useHotkeys('CTRL + G', () => atm.go())
+
+  // exhibition mode - program
+  function startExhibitionProgramReal() {
+    $offlineMode.set(true)
+    $exhibitionMode.set(true)
+
+    $videoMode.set(false)
+    $canPlay.set(true)
+    automator.sync({force: true})
+    fullscreen()
+
+    go({to: '/zero'})
+  }
+
+  // exhibition mode - video
+  function startExhibitionVideo() {
+    go({to: '/video'})
+
+    $exhibitionMode.set(true)
+
+    $videoMode.set(true)
+    $canPlay.set(true)
+    automator.sync({force: true})
+    fullscreen()
+  }
+
+  useEffect(() => {
+    const params = window.location.search
+
+    if (params.includes('screen=left')) {
+      startExhibitionVideo()
+    } else if (params.includes('screen=right')) {
+      startExhibitionProgramReal()
+    }
+  })
 
   return null
 }
