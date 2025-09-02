2f8394ace7f55be992d1dc99809379bce3afcbd1
Phoomparin Mano <phoomparin@gmail.com>
2025-09-02 09:59:25 +0700

    increase tick interval

--- ui/src/components/WaitingScreen.tsx
+++ ui/src/components/WaitingScreen.tsx
@@ -33,7 +33,7 @@ export function WaitingRoomScreen() {
   useEffect(() => {
     const timer = setInterval(() => {
       tick()
-    }, 500)
+    }, 100)
 
     return () => clearInterval(timer)
   }, [tick])
