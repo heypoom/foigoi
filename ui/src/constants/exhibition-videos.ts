71b7f9e9a96eda319754b465de535799bc1245d5
Phoomparin Mano <phoomparin@gmail.com>
2025-11-02 22:40:59 +0700

    use different ws endpoint

--- ui/src/constants/exhibition-videos.ts
+++ ui/src/constants/exhibition-videos.ts
@@ -1,5 +1,5 @@
 export const EXHIBITION_VIDEO_SOURCES = {
-  lecture: "https://rui-an-cn-api.poom.dev/rui-an-dec-22-fast.mp4",
+  lecture: "http://121.37.180.139:8080/rui-an-dec-22-fast.mp4",
 
-  programVideo: "https://rui-an-cn-api.poom.dev/rui-an-dec-22-program-v2.mp4",
+  programVideo: "http://121.37.180.139:8080/rui-an-dec-22-program-v2.mp4",
 } as const;
