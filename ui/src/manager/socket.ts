7785d2b6a30466407a9aa1047bf46de52c5c957c
Phoomparin Mano <phoomparin@gmail.com>
2025-11-06 05:00:42 +0700

    use rui-an-api.vanguardgallery.com

--- ui/src/manager/socket.ts
+++ ui/src/manager/socket.ts
@@ -20,8 +20,8 @@ const generationTimeouts: Record<string, number> = {
   P4: 20 * 1000,
 };
 
-const EXHIBITION_ENDPOINT = "ws://121.37.180.139:8080/ws";
-const LIVE_LECTURE_ENDPOINT = "ws://121.37.180.139:8080/ws";
+const EXHIBITION_ENDPOINT = "wss://rui-an-api.vanguardgallery.com/ws";
+const LIVE_LECTURE_ENDPOINT = "wss://rui-an-api.vanguardgallery.com/ws";
 
 class SocketManager {
   sock: WebSocket;
